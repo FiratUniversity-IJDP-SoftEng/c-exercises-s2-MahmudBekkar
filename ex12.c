e668e124a9ec8def89485a42cd7a4a84586084fc
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-11-04 10:59:14 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    do {
+        x = get_int("Enter number less than 50: ");
+    }while (x < 0 || x >= 50);
+
+    long long m = 1;
+
+    for (int y = 1; y <= x; y++) {
+        m *= y;
+    }
+    printf("%d! = %lld\n", x, m);
+}
 

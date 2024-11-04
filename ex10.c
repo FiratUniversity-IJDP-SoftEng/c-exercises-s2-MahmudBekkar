7498022aa4c5a0a7caadbae5beb61a83df26a17f
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-11-04 10:42:28 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    for (int x = 1; x <= 10; x++) {
+
+        for (int y = 1; y <= 10; y++) {
+            printf("%-4d", (x * y));
+        }
+        printf("\n");
+    }
+
+}
 

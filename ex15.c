53871665a43108ffc42ebbe4a1428ba515a597c6
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-11-04 12:58:15 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+void print_diamond(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+int main(void) {
+    int n;
+    n = get_int("Enter the number of rows for the diamond: ");
+    print_diamond(n);
+}
 

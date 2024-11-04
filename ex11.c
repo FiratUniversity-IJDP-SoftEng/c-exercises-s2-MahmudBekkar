a572f67fb4424cf084243b0d2ea5b150b01fbe86
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-11-04 10:47:37 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    int x = 5;
+    for (int y = 0; y < x; y++) {
+        for (int m = 0; m < x; m++){
+
+            if (m == y || m == (x - 1 - y)){
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
 

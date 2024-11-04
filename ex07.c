ad6482609e485929c878523a1c82372415be8f1d
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-11-04 10:06:57 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+int main() {
+    for (int x = 1; x <= 10; x++) {
+        int y1 = x * 5;
+        int y2 = 100 - ((x - 1) * 10 + 1);
+        int y3 = x;
+        printf("%-3d %-3d %-3d\n", y1, y2, y3);
+    }
+}
